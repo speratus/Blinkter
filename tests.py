150d3fd96c3831fc2dbe8abef9849c1c3a9e370b
Andrew Luchuk <spaxumof@gmail.com>
2019-09-16 10:48:35 -0400

    Added Primary color setting tests

--- tests.py
+++ tests.py
@@ -25,6 +25,8 @@ import threading
 from unittest import mock
 
 #import blinkt
+import sys
+sys.modules['blinkt'] = mock.MagicMock()
 
 from blinkter import BlinktBoard, Pixel, LED
 
@@ -45,13 +47,51 @@ class BasicPixelTests(unittest.TestCase):
 #        mock_show.assert_called()
 #        mock_release.assert_called()
 
+
 class ColorSettingTests(unittest.TestCase):
-    def SetUp(self):
+    def setUp(self):
         self.board = BlinktBoard()
-        self.pixel = self.board.get_pixe(0)
+        self.pixel = self.board.get_pixel(0)
         
     def test_correct_pixel(self):
-        pass
+        self.assertEqual(self.pixel.addr, 0, msg='The Pixel address was incorrect!')
+
+    @mock.patch.object(Pixel, 'draw', autospec=True)
+    def test_black(self, mock_draw):
+        self.pixel.set_color(14, 14, 14)
+        mock_draw.assert_called()
+        self.pixel.black()
+        black = [0, 0, 0]
+        self.assertEqual(self.pixel.rgb, black, msg=f'pixel.black() failed. {self.pixel.rgb} did not equal {black}.')
+        mock_draw.assert_called()
+
+    @mock.patch.object(Pixel, 'draw', autospec=True)
+    def test_white(self, mock_draw):
+        self.pixel.white()
+        mock_draw.assert_called()
+        white = [255,255,255]
+        self.assertEqual(self.pixel.rgb, white, msg=f'pixel.white() failed. {self.pixel.rgb} does not equal {white}.')
+
+    @mock.patch.object(Pixel, 'draw', autospec=True)
+    def test_red(self, mock_draw):
+        self.pixel.red()
+        mock_draw.assert_called()
+        red = [255, 0, 0]
+        self.assertEqual(self.pixel.rgb, red, msg=f'pixel.red() failed. {self.pixel.rgb} does not equal {red}.')
+
+    @mock.patch.object(Pixel, 'draw', autospec=True)
+    def test_green(self, mock_draw):
+        self.pixel.green()
+        mock_draw.assert_called()
+        green = [0, 255, 0]
+        self.assertEqual(self.pixel.rgb, green, msg=f'pixel.red() failed. {self.pixel.rgb} does not equal {green}.')
+
+    @mock.patch.object(Pixel, 'draw', autospec=True)
+    def test_blue(self, mock_draw):
+        self.pixel.blue()
+        mock_draw.assert_called()
+        blue = [0, 0, 255]
+        self.assertEqual(self.pixel.rgb, blue, msg=f'pixel.red() failed. {self.pixel.rgb} does not equal {blue}.')
 
 if __name__ == '__main__':
     unittest.main()
